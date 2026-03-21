9165331b92eb0730ae381d1bbd6ad36270a88e7f
Vega <alexkarpytskyi@gmail.com>
2026-03-21 20:57:52 +0200

    fix movie type fields

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,11 +1,15 @@
-export type Movie = {
+export interface Movie {
   id: number;
   title: string;
   poster_path: string;
-};
+  backdrop_path: string;
+  overview: string;
+  release_date: string;
+  vote_average: number;
+}
 
-export type MovieResponse = {
+export interface MovieResponse {
   page: number;
   results: Movie[];
   total_pages: number;
-};
+}
