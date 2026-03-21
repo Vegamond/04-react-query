0cbb4cd070f033f0a66953de5253cb2af1e5a67b
Vega <alexkarpytskyi@gmail.com>
2026-03-21 21:37:24 +0200

    fix movie type fields

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import 'modern-normalize';
-import App from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "modern-normalize";
+import App from "./components/App/App";
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
