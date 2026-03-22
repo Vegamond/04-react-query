979ce2bf8fd3e084aaf48b2b360464cdd396ff9d
Vega <80153765+Vegamond@users.noreply.github.com>
2026-03-22 22:58:32 +0200

    Update MovieGrid.tsx

--- src/components/MovieGrid/MovieGrid.tsx
+++ src/components/MovieGrid/MovieGrid.tsx
@@ -1,25 +1,36 @@
-import type { Movie } from "../../types/movie";
-import css from "./MovieGrid.module.css";
+import type { Movie } from '../../types/movie';
+import css from './MovieGrid.module.css';
 
-interface MovieGridProps {
+export interface MovieGridProps {
   movies: Movie[];
   onSelect: (movie: Movie) => void;
 }
 
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+const FALLBACK_IMAGE =
+  'https://via.placeholder.com/300x450?text=No+Image';
+
 export default function MovieGrid({ movies, onSelect }: MovieGridProps) {
   return (
     <ul className={css.grid}>
-      {movies.map((movie) => (
-        <li key={movie.id} onClick={() => onSelect(movie)}>
-          <div className={css.card}>
+      {movies.map(movie => (
+        <li key={movie.id} className={css.item}>
+          <button
+            className={css.card}
+            type="button"
+            onClick={() => onSelect(movie)}
+          >
             <img
               className={css.image}
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              src={
+                movie.poster_path
+                  ? `${IMAGE_BASE_URL}${movie.poster_path}`
+                  : FALLBACK_IMAGE
+              }
               alt={movie.title}
-              loading="lazy"
             />
             <h2 className={css.title}>{movie.title}</h2>
-          </div>
+          </button>
         </li>
       ))}
     </ul>
